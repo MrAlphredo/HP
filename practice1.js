851a0636f6d720647ae88314c8128713597ef0c2
MrAlphredo <60201037+MrAlphredo@users.noreply.github.com>
2021-07-16 22:56:36 -0400

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function myFunction() {
+    var btn = document.createElement('input');
+    btn.setAttribute('type', 'button'); // input element of type button
+    btn.setAttribute('value', 'FINISH GAME');
+    btn.onclick = sf;
+    document.body.appendChild(btn);
+};
+function func1() {
+    document.getElementById("test").innerHTML = "HMMMMMMMMM!"
+
+}
